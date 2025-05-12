bf8b4e318db83aab9c5ceb8c99960fc36d79351b
Pratap Padhy <pratappadhy2005@gmail.com>
2025-05-12 22:07:11 +1000

    Updated few pages to reflect correct content

--- src/components/skillsSection/SkillsText.jsx
+++ src/components/skillsSection/SkillsText.jsx
@@ -3,9 +3,7 @@ const SkillsText = () => {
     <div className="flex flex-col items-center mt-[100px]">
       <h2 className="text-6xl text-cyan mb-10">My Skills</h2>
       <p className="text-lg text-center">
-        I not only work with these technologies but excellent in using them with
-        best practices to deliver high-quality results, I have been working with
-        all these skills to build my portfolio projects
+        Experienced in building scalable and secure enterprise applications using Java and Spring Boot, with a strong focus on microservices architecture. Proficient in deploying and managing cloud-native solutions on AWS and Azure, leveraging Docker and Kubernetes for container orchestration. Skilled in Node.js for backend scripting and API development, and well-versed in Agile methodologies to drive iterative delivery. Expertise in Terraform for Infrastructure as Code (IaC), enabling consistent and automated cloud infrastructure provisioning.
       </p>
     </div>
   );
